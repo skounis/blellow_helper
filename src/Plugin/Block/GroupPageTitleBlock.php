d3fb62bc27e5f004282cebc1edcf5d57464cd001
Stavros Kounis (lu) <skounis@gmail.com>
2019-07-03 16:18:13 +0200

    chore(Group Page Title) Remove dead code

--- src/Plugin/Block/GroupPageTitleBlock.php
+++ src/Plugin/Block/GroupPageTitleBlock.php
@@ -4,18 +4,14 @@ declare(strict_types = 1);
 
 namespace Drupal\blellow_helper\Plugin\Block;
 
+use Drupal\Core\Access\AccessResult;
+
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\TitleBlockPluginInterface;
-use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
-use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\ContextAwarePluginInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\group\Entity\GroupInterface;
-use False\MyClass;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -34,21 +30,6 @@ class GroupPageTitleBlock extends BlockBase implements ContainerFactoryPluginInt
 
   use StringTranslationTrait;
 
-
-  /**
-   * Stores the configuration factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected $configFactory;
-
-  /**
-   * The current route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $currentRouteMatch;
-
   /**
    * The page title: a string (plain title) or a render array (formatted title).
    *
@@ -65,16 +46,9 @@ class GroupPageTitleBlock extends BlockBase implements ContainerFactoryPluginInt
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The factory for configuration objects.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
-   *   The current route match.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, RouteMatchInterface $current_route_match) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->configFactory = $config_factory;
-    $this->currentRouteMatch = $current_route_match;
   }
 
   /**
@@ -84,9 +58,7 @@ class GroupPageTitleBlock extends BlockBase implements ContainerFactoryPluginInt
     return new static(
       $configuration,
       $plugin_id,
-      $plugin_definition,
-      $container->get('config.factory'),
-      $container->get('current_route_match')
+      $plugin_definition
     );
   }
 
@@ -122,7 +94,7 @@ class GroupPageTitleBlock extends BlockBase implements ContainerFactoryPluginInt
    */
   protected function blockAccess(AccountInterface $account) {
     $subtitle = $this->getSubtitle();
-    return \Drupal\Core\Access\AccessResult::allowedIf(!empty($subtitle));
+    return AccessResult::allowedIf(!empty($subtitle));
   }
 
   private function getSubtitle() {
