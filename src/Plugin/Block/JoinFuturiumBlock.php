05627a1b089736968578c2c4d4889694368c7210
Stavros Kounis (lu) <skounis@gmail.com>
2019-09-18 13:00:00 +0200

    feat(welcome / join) Load values from configuration

--- src/Plugin/Block/JoinFuturiumBlock.php
+++ src/Plugin/Block/JoinFuturiumBlock.php
@@ -7,6 +7,7 @@ namespace Drupal\blellow_helper\Plugin\Block;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\RendererInterface;
@@ -22,7 +23,43 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   category = @Translation("Cnect Corporate blocks"),
  * )
  */
-class JoinFuturiumBlock extends BlockBase {
+class JoinFuturiumBlock extends BlockBase  implements ContainerFactoryPluginInterface{
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Construct the footer block object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -35,6 +72,11 @@ class JoinFuturiumBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $cache = new CacheableMetadata();
+    $cache->addCacheContexts(['languages:language_interface']);
+    $config = $this->configFactory->get('blellow_helper.data.welcome');
+
+    $cache->addCacheableDependency($config);
     $build['#theme'] = 'cnect_corporate_blocks_join_futurium';
 
     $build['#attached'] = [
@@ -42,6 +84,11 @@ class JoinFuturiumBlock extends BlockBase {
         'blellow_helper/join_futurium',
       ],
     ];
+
+    NestedArray::setValue($build, ['#corporate_welcome', 'join', 'title'], $config->get('join_title'));
+    NestedArray::setValue($build, ['#corporate_welcome', 'join', 'body'], $config->get('join_body'));
+    NestedArray::setValue($build, ['#corporate_welcome', 'join', 'call'], $config->get('join_call'));
+    NestedArray::setValue($build, ['#corporate_welcome', 'join', 'target'], $config->get('join_target'));
 
     return $build;
   }
--- src/Plugin/Block/WelcomeBlock.php
+++ src/Plugin/Block/WelcomeBlock.php
@@ -7,9 +7,9 @@ namespace Drupal\blellow_helper\Plugin\Block;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -22,7 +22,43 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   category = @Translation("Cnect Corporate blocks"),
  * )
  */
-class WelcomeBlock extends BlockBase {
+class WelcomeBlock extends BlockBase implements ContainerFactoryPluginInterface{
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Construct the footer block object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -35,13 +71,25 @@ class WelcomeBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build['#theme'] = 'cnect_corporate_blocks_welcome';
+    $cache = new CacheableMetadata();
+    $cache->addCacheContexts(['languages:language_interface']);
+    $config = $this->configFactory->get('blellow_helper.data.welcome');
 
+    $cache->addCacheableDependency($config);
+
+    $build['#theme'] = 'cnect_corporate_blocks_welcome';
     $build['#attached'] = [
       'library' => [
         'blellow_helper/welcome',
       ],
     ];
+
+    NestedArray::setValue($build, ['#corporate_welcome', 'welcome', 'title'], $config->get('welcome_title'));
+    NestedArray::setValue($build, ['#corporate_welcome', 'welcome', 'body'], $config->get('welcome_body'));
+    NestedArray::setValue($build, ['#corporate_welcome', 'welcome', 'call'], $config->get('welcome_call'));
+    NestedArray::setValue($build, ['#corporate_welcome', 'welcome', 'target'], $config->get('welcome_target'));
+
+    $cache->applyTo($build);
 
     return $build;
   }
