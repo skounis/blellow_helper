2d2a95e81cd0aefb5e515d3343d00961d1d5a0e0
Stavros Kounis (lu) <skounis@gmail.com>
2019-10-01 15:39:21 +0200

    feat(links) reprocess links
    Remove deads

--- src/Plugin/Block/SelectLanguagesBlock.php
+++ src/Plugin/Block/SelectLanguagesBlock.php
@@ -4,13 +4,14 @@ declare(strict_types = 1);
 
 namespace Drupal\blellow_helper\Plugin\Block;
 
-use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Path\PathMatcherInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -22,15 +23,97 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   category = @Translation("Cnect Corporate blocks"),
  * )
  */
-class SelectLanguagesBlock extends BlockBase {
+class SelectLanguagesBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The path matcher.
+   *
+   * @var \Drupal\Core\Path\PathMatcherInterface
+   */
+  protected $pathMatcher;
+
+  /**
+   * Constructs an LanguageBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   * @param \Drupal\Core\Path\PathMatcherInterface $path_matcher
+   *   The path matcher.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $language_manager, PathMatcherInterface $path_matcher) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->languageManager = $language_manager;
+    $this->pathMatcher = $path_matcher;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('language_manager'),
+      $container->get('path.matcher')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    $access = $this->languageManager->isMultilingual() ? AccessResult::allowed() : AccessResult::forbidden();
+    return $access->addCacheTags(['config:configurable_language_list']);
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build['#theme'] = 'cnect_corporate_blocks_select_languages';
+    $build = [];
+    $route_name = $this->pathMatcher->isFrontPage() ? '<front>' : '<current>';
+    $links = $this->languageManager->getLanguageSwitchLinks(LanguageInterface::TYPE_INTERFACE, Url::fromRoute($route_name));
+
+    if (isset($links->links)) {
+      $build = [
+        // HINT: prefix the theme with `links__` and get the preprocess  of the
+        // links at no extra cost. If not `template_preprocess_links($variables)` should be called.
+        '#theme' => 'cnect_corporate_blocks_select_languages',
+        '#links' => $links->links,
+        '#attributes' => [
+          'class' => [
+            "language-switcher-{$links->method_id}",
+          ],
+        ],
+        '#set_active_class' => TRUE,
+      ];
+    }
 
     return $build;
   }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @todo Make cacheable in https://www.drupal.org/node/2232375.
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
 
 }
