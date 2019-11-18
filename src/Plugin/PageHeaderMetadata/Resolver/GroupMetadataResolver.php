354a9561c409e498ed1eeceb6a8a31dd2c821b09
Stavros Kounis (lu) <skounis@gmail.com>
2019-11-18 15:13:39 +0100

    fix(url) Use absolute URL to group

--- src/Plugin/PageHeaderMetadata/Resolver/GroupMetadataResolver.php
+++ src/Plugin/PageHeaderMetadata/Resolver/GroupMetadataResolver.php
@@ -11,17 +11,19 @@ use Drupal\blellow_helper\Plugin\PageHeaderMetadata\Helper\FutGroupHelper;
  */
 class GroupMetadataResolver extends MetadataResolverBase {
 
-
   public function _getMetadata() {
     $group = $this->entities->getPrimary();
     $node = $this->entities->getRelated();
     $visual_identity = FutGroupHelper::getVisualIdentity($group);
     $group_operations = FutGroupHelper::getGroupOperations($group);
     $subtitle = isset($node) ? $node->label() : NULL;
-    $groupUrl = $group->toUrl();
+    $groupUrl = $group->toUrl('canonical', [
+      'absolute' => TRUE,
+      'language' => \Drupal::languageManager()->getCurrentLanguage(),
+    ]);
     $url = [];
     if (!empty($groupUrl)) {
-      $url =  $groupUrl->getInternalPath();
+      $url = $groupUrl->toString();
     }
 
     $actions = FutGroupHelper::groupActionsToECLArray($group_operations);
