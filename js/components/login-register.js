4654f952e198591b0940f57bb9679d7ef3eef6a9
Stavros Kounis <skounis@gmail.com>
2020-01-27 14:39:52 +0100

    Update login-register.js

--- js/components/login-register.js
+++ js/components/login-register.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
-    ECL.initExpandables('#cnect-expandable-button-login');
-    ECL.dropdown('#cnect-dropdown-login');
+    ECL.initExpandables('#cnect-expandable-button-cas-login');
+    ECL.dropdown('#cnect-dropdown-cas-login');
 
-    ECL.initExpandables('#cnect-expandable-button-register');
-    ECL.dropdown('#cnect-dropdown-register');
+    ECL.initExpandables('#cnect-expandable-button-user-register');
+    ECL.dropdown('#cnect-dropdown-user-register');
 });
